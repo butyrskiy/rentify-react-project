e8dde0aeb6d77197b3255a267e7aec8de06533ec
Denis Butyrskiy <d.butyrskiy@gmail.com>
2024-02-12 22:50:20 +0400

    complete sign up page ui

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,5 +1,40 @@
+import { Link } from "react-router-dom";
+
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return (
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Регистрация</h1>
+      <form className="flex flex-col gap-4">
+        <input
+          type="text"
+          id="username"
+          placeholder="username"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="email"
+          id="email"
+          placeholder="email"
+          className="border p-3 rounded-lg"
+        />
+        <input
+          type="password"
+          id="password"
+          placeholder="password"
+          className="border p-3 rounded-lg"
+        />
+        <button className="bg-slate-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80">
+          Зарегистрироваться
+        </button>
+      </form>
+      <div className="flex gap-2 mt-4">
+        <p>Уже есть аккаунт?</p>
+        <Link to={"/sign-in"}>
+          <span className="text-blue-700">Войти</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default SignUp;
