3bc8254a8a071eb00608f6b25c38c28a569ec5f3
Denis Butyrskiy <d.butyrskiy@gmail.com>
2024-02-12 15:47:52 +0400

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import { error, log } from "console";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
 const app = express();
 
